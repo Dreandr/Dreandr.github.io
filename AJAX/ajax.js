8448f60dd876f5a690479d18f6ab08b11182dee5
Den Khor <dreandr@mail.ru>
2020-06-09 18:43:58 +0300

    added index.html

---
+++ AJAX/ajax.js
@@ -0,0 +1,25 @@
+let xhr = new XMLHttpRequest();
+
+xhr.open('GET', 'zivuch.github.io/ajax/data.html');
+xhr.send();
+
+xhr.onload = function() {
+  if (xhr.status != 200) { // analyze HTTP status of the response
+    alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+  } else { // show the result
+    alert(`Done, got ${xhr.response}`); // responseText is the server
+  }
+};
+
+xhr.onprogress = function(event) {
+  if (event.lengthComputable) {
+    alert(`Received ${event.loaded} of ${event.total} bytes`);
+  } else {
+    alert(`Received ${event.loaded} bytes`); // no Content-Length
+  }
+
+};
+
+xhr.onerror = function() {
+  alert("Request failed");
+};
